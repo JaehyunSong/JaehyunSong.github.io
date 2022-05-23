3242c4b5f71ce6b21053aab33087e0aa13c23cac
Jaehyun Song <tintstyle@gmail.com>
2022-05-23 13:20:07 +0900

    no message

---
+++ tutorial/R/_common.R
@@ -0,0 +1,9 @@
+golem::detach_all_attached()
+rm(list = ls())
+pacman::p_load(tidyverse, knitr, kableExtra, gt)
+options(tinytex.latexmk.emulation = FALSE)
+knitr::opts_chunk$set(attr.source = ".numberLines", 
+                      dev         = "ragg_png",
+                      fig.align   = "center",
+                      dpi         = 300,
+                      fig.pos     = "H")
